c1552d8c360a87ac9371174dd2e5d9c8702dd8b2
fofackClinton <fofackclinton23@icloud.com>
2023-07-07 10:14:48 +0200

    recuperation de l'image en bd

--- src/Controller/PriduitsController.php
+++ src/Controller/PriduitsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Priduits;
 use App\Form\ImageType;
+use App\Repository\PriduitsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,13 +24,13 @@ class PriduitsController extends AbstractController
         ]);
     }
 
-    #[Route('/priduits/new', name: 'app_priduitsNew')]
-    public function new(Request $request,
+    #[Route('/priduits/new', name: 'app_priduitsNew', methods : ['GET','POST'])]
+    public function newProduits(Request $request,
     EntityManagerInterface $manager
    ) : Response
    {
-     $produits = new Priduits();
-     $form = $this->createForm(ImageType::class, $produits);
+     $priduits = new Priduits();
+     $form = $this->createForm(ImageType::class, $priduits);
      $form->handleRequest($request);
      if($form->isSubmitted() && $form->isValid()){
         $clinton = $form->getData();
@@ -43,5 +44,30 @@ class PriduitsController extends AbstractController
     ]);
 
    }
+
+   #[Route('/priduits/update/{id<[0-9]+>}', name: 'app_priduitsUp', methods : ['GET','POST'])]
+   public function updateProduits(Priduits $pro,
+    Request $request,
+   EntityManagerInterface $manager
+   
+  ) : Response
+  {
+    //$priduits = new Priduits();
+     //$form = $this->createForm(ImageType::class, $priduits);
+    //$produits = $repository->findBy(["id"=>'$id']);
+    $form = $this->createForm(ImageType::class, $pro);
+    $form->handleRequest($request);
+    if($form->isSubmitted() && $form->isValid()){
+       $clinton = $form->getData();
+       $manager->persist($clinton);
+       $manager->flush();
+       return $this->redirectToRoute('app_priduitsNew');
+    }
+
+    return $this->render('priduits/update.html.twig', [
+       'form' => $form->createView()
+   ]);
+
+  }
 
 }
--- src/Entity/Priduits.php
+++ src/Entity/Priduits.php
@@ -34,14 +34,18 @@ class Priduits
 
     public function setImageFile(?File $imageFile = null): void
     {
-        $this->imageFile = $imageFile;
+         $this->imageFile = $imageFile;
 
         if (null !== $imageFile) {
             // It is required that at least one field changes if you are using doctrine
             // otherwise the event listeners won't be called and the file is lost
             $this->setUpdatedAt(new \DateTimeImmutable);
-            $this->setCeatedAt(new \DateTimeImmutable);
+           
         }
+        
+            $this->setCeatedAt(new \DateTimeImmutable);
+        
+        
     }
 
     public function getImageFile(): ?File
